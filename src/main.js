950db225b2a8fc70c313fc620030a6fedca2ee84
Julieta Miho Yamate <julietamiho@gmail.com>
2023-05-07 11:09:49 -0300

    questao1 e 2

---
+++ src/main.js
@@ -0,0 +1,8 @@
+let numero = prompt("informe o numero")
+let questao1 = document.querySelector ("#questao1")
+questao1.innerHTML = `O resultado é: ${ numero * 2}`
+
+let valorJantar = parseInt(prompt("Qual valor da conta ? "))
+const valorGarcom = valorJantar * 0.10
+let questao02 = document.querySelector("#questao02"); // Pega id da tag div de forma dinamica
+questao02.innerHTML = `O valor da Jantar é: ${valorJantar}
